4c0614bf6012fa8fa10d0398086f706e17b05a68
Jan <thetisiboth@gmail.com>
2021-05-28 14:05:57 +0200

    Remove unused part from script

--- start-envoy.sh
+++ start-envoy.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-if ! [ -x "$(command -v protoc)" ] ; then
-    echo "you do not seem to have the protoc executable on your path"
-    echo "we need protoc to generate a service defintion (*.pb file) that envoy can understand"
-    echo "download the precompiled protoc executable and place it in somewhere in your systems PATH!"
-    echo "goto: https://github.com/protocolbuffers/protobuf/releases/latest"
-    echo "choose:"
-    echo "       for linux:   protoc-3.6.1-linux-x86_64.zip"
-    echo "       for windows: protoc-3.6.1-win32.zip"
-    echo "       for mac:     protoc-3.6.1-osx-x86_64.zip"
-    exit 1
-fi
-
 # now we can start envoy in a docker container and map the configuration and service definition inside
 # we use --network="host" so that envoy can access the grpc service at localhost:<port>
 # the envoy-config.yml has configured envoy to run at port 51051, so you can access the HTTP/JSON
